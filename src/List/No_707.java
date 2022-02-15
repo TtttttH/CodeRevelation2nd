d6680d2c9e7b74d8f1f9f4f989abe089782efba0
TtttttH <574173437@qq.com>
2022-02-15 10:25:13 +0000

    No.707 设计链表 大体思路：设置一个size初始化0，设置一个虚拟头结点

---
+++ src/List/No_707.java
@@ -0,0 +1,78 @@
+package List;
+
+import sun.dc.pr.PRError;
+
+/**
+ * No.707 Design LinkedList (middle)
+ */
+public class No_707 {
+    public class MyLinkedList {
+        int size;
+        ListNode head;
+        public MyLinkedList(){
+            this.size = 0;
+            head = new ListNode(0);
+        };
+
+        public int get(int index) {
+            if (index < 0 || index >= size) {
+                return -1;
+            }
+
+            ListNode cur = head;
+            while (index >= 0) {
+                cur = cur.next;
+                index --;
+            }
+
+            return cur.val;
+        }
+
+        public void addAtHead(int val) {
+            ListNode node = new ListNode(val);
+            ListNode temp = head.next;
+            head.next = node;
+            node.next = temp;
+            size ++;
+        }
+
+        public void addAtTail(int val) {
+            ListNode node = new ListNode(val);
+            ListNode cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+
+            cur.next = node;
+            size ++;
+        }
+
+        public void addAtIndex(int index, int val) {
+            if (index < 0) {
+                index = 0;
+            }
+            if (index > size) {
+                return;
+            }
+            ListNode preNode = head;
+            for (int i = 0; i < index; i ++) {
+                preNode = preNode.next;
+            }
+            ListNode node = new ListNode(val);
+            node.next = preNode.next;
+            preNode.next = node;
+            size ++;
+        }
+
+        public void deleteAtIndex(int index) {
+            if (index >= 0 && index < size) {
+                ListNode preNode = head;
+                for (int i = 0; i < index; i ++) {
+                    preNode = preNode.next;
+                }
+                preNode.next = preNode.next.next;
+                size --;
+            }
+        }
+    }
+}
