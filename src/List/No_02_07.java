326c7a1dd92c370c8160833c248ec9f648b8f89f
TtttttH <574173437@qq.com>
2022-02-16 01:32:42 +0000

    No.02_07 链表相交位置 快慢指针法

---
+++ src/List/No_02_07.java
@@ -0,0 +1,40 @@
+package List;
+
+/**
+ * 面试题02.07. Intersection of Two Linked Lists LCCI
+ */
+public class No_02_07 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int countA = 0;
+        int countB = 0;
+        while (curA!= null) {
+            countA ++;
+            curA = curA.next;
+        }
+
+        while (curB != null) {
+            countB ++;
+            curB = curB.next;
+        }
+        curA = headA;
+        curB = headB;
+        if (countA > countB) {
+            for (int i = 0; i < countA - countB; i ++) {
+                curA = curA.next;
+            }
+        } else {
+            for (int i = 0; i < countB - countA; i++) {
+                curB = curB.next;
+            }
+        }
+
+        while (curA != curB) {
+            curA = curA.next;
+            curB = curB.next;
+        }
+
+        return curA;
+    }
+}
