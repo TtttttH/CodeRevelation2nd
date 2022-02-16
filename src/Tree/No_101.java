2e899bf7d42b84392d22d9189ae117673b51c669
TtttttH <574173437@qq.com>
2022-02-16 00:24:20 +0000

    No.101 对称二叉树

---
+++ src/Tree/No_101.java
@@ -0,0 +1,30 @@
+package Tree;
+
+
+/**
+ * No.101 Symmetric Tree(easy)
+ */
+public class No_101 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left != null && right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        }
+
+        return false;
+    }
+}
