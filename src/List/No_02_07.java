76a218abd9b942360f44119b9c8f9c58a85e58b7
TtttttH <574173437@qq.com>
2022-02-16 01:35:56 +0000

    No.02_07 链表相交位置 数学方法，注释处

--- src/List/No_02_07.java
+++ src/List/No_02_07.java
@@ -37,4 +37,16 @@ public class No_02_07 {
 
         return curA;
     }
+
+    //数学方法,类似环形链表思想
+//    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//        ListNode curA = headA;
+//        ListNode curB = headB;
+//        while (curA != curB) {
+//            curA = curA == null ? headB : curA.next;
+//            curB = curB == null ? headA : curB.next;
+//        }
+//
+//        return curA;
+//    }
 }
