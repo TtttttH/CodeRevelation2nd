924cc080ee1f9a81ade5f23559427c0c15f5b737
TtttttH <574173437@qq.com>
2022-02-17 07:40:38 +0000

    No.438 优化时间复杂度做法，但是运行时间变长了= =

--- src/Hash/No_438.java
+++ src/Hash/No_438.java
@@ -34,4 +34,50 @@ public class No_438 {
 
         return results;
     }
+
+    public List<Integer> findAnagrams_2(String s, String p) {
+        List<Integer> results = new ArrayList<>();
+        if (s == null || s.length() < p.length()) {
+            return results;
+        }
+
+        int pLen = p.length();
+        int sLen = s.length();
+        int differ = 0;
+        int[] count = new int[26];
+        for (int i = 0; i < pLen; i ++) {
+            count[s.charAt(i) - 'a'] ++;
+            count[p.charAt(i) - 'a'] --;
+        }
+
+        for (int i : count) {
+            if (i != 0) {
+                differ ++;
+            }
+        }
+
+        if (differ == 0) {
+            results.add(0);
+        }
+        for (int i = 0; i < sLen - pLen; i ++) {
+            if (count[s.charAt(i) - 'a'] == 1) {
+                differ --;
+            } else if (count[s.charAt(i) - 'a'] == 0) {
+                differ ++;
+            }
+            count[s.charAt(i) - 'a'] --;
+            if (count[s.charAt(i + pLen) - 'a'] == 0) {
+                differ ++;
+            } else if (count[s.charAt(i + pLen) - 'a'] == -1) {
+                differ --;
+            }
+            count[s.charAt(i + pLen) - 'a'] ++;
+
+            if (differ == 0) {
+                results.add(i + 1);
+            }
+        }
+
+        return results;
+    }
 }
