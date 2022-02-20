b9badeac6472dcc9606fdcc8db76d9b65fde7237
TtttttH <574173437@qq.com>
2022-02-20 09:54:00 +0000

    剑指II 58 左旋转字符串 防止面试不允许切片运算

--- src/String/Offer_58.java
+++ src/String/Offer_58.java
@@ -5,6 +5,11 @@ package String;
  */
 public class Offer_58 {
     public String reverseLeftWords(String s, int n) {
-        return s.substring(n) + s.substring(0, n);
+        StringBuilder res = new StringBuilder();
+        for(int i = n; i < s.length(); i++)
+            res.append(s.charAt(i));
+        for(int i = 0; i < n; i++)
+            res.append(s.charAt(i));
+        return res.toString();
     }
 }
