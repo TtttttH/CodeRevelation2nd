f5728239642c889b4982137a87dc5d04108b04d3
TtttttH <574173437@qq.com>
2022-02-23 17:15:43 +0000

    No.347 topK 优先级队列大顶堆问题,微软题 O(nlogk)

---
+++ src/StackQueue/No_347.java
@@ -0,0 +1,36 @@
+package StackQueue;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * No.347 前K个高频元素 (middle) 微软题
+ */
+public class No_347 {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        });
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            queue.add(entry);
+        }
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i ++) {
+            result[i] = queue.poll().getKey();
+        }
+
+        return result;
+    }
+}
