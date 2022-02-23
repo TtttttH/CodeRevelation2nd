193e260ee03627ab9d1d8ec3d667b825e50647f4
TtttttH <574173437@qq.com>
2022-02-23 17:17:40 +0000

    No.347 topK 补充了一下堆排细节以及队列函数优化

--- src/StackQueue/No_347.java
+++ src/StackQueue/No_347.java
@@ -18,12 +18,12 @@ public class No_347 {
         PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(new Comparator<Map.Entry<Integer, Integer>>() {
             @Override
             public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return o2.getValue() - o1.getValue();
+                return o2.getValue() - o1.getValue(); //这里是构建大顶堆，也可以用小顶堆o1 - o2
             }
         });
 
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            queue.add(entry);
+            queue.offer(entry);
         }
 
         int[] result = new int[k];
