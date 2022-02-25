86fe94c8d38df2c4fea7bf884a8ff459b5f06673
TtttttH <574173437@qq.com>
2022-02-25 18:00:10 +0000

    No.111 二叉树的最小深度 层序遍历 BFS

---
+++ src/Tree/No_111.java
@@ -0,0 +1,29 @@
+package Tree;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * No.111 二叉树的最小深度(easy)
+ */
+public class No_111 {
+    public int minDepth(TreeNode root) {
+        //BFS
+        if (root == null) return 0;
+        int depth = 0;
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.offer(root);
+        while (!deque.isEmpty()) {
+            depth ++;
+            int size = deque.size();
+            for (int i = 0; i < size; i ++) {
+                TreeNode cur = deque.poll();
+                if (cur.left == null && cur.right == null) return depth;
+                if (cur.left != null) deque.offer(cur.left);
+                if (cur.right != null) deque.offer(cur.right);
+            }
+        }
+
+        return depth;
+    }
+}
