fc7837bbec6535fb4772653551247fcbbf226caf
TtttttH <574173437@qq.com>
2022-02-25 18:18:41 +0000

    No.111 二叉树的最小深度 DFS递归，O(n) 递归思想比较难想到

--- src/Tree/No_111.java
+++ src/Tree/No_111.java
@@ -9,21 +9,26 @@ import java.util.Deque;
 public class No_111 {
     public int minDepth(TreeNode root) {
         //BFS
+//        if (root == null) return 0;
+//        int depth = 0;
+//        Deque<TreeNode> deque = new ArrayDeque<>();
+//        deque.offer(root);
+//        while (!deque.isEmpty()) {
+//            depth ++;
+//            int size = deque.size();
+//            for (int i = 0; i < size; i ++) {
+//                TreeNode cur = deque.poll();
+//                if (cur.left == null && cur.right == null) return depth;
+//                if (cur.left != null) deque.offer(cur.left);
+//                if (cur.right != null) deque.offer(cur.right);
+//            }
+//        }
+//
+//        return depth;
+        //DFS
         if (root == null) return 0;
-        int depth = 0;
-        Deque<TreeNode> deque = new ArrayDeque<>();
-        deque.offer(root);
-        while (!deque.isEmpty()) {
-            depth ++;
-            int size = deque.size();
-            for (int i = 0; i < size; i ++) {
-                TreeNode cur = deque.poll();
-                if (cur.left == null && cur.right == null) return depth;
-                if (cur.left != null) deque.offer(cur.left);
-                if (cur.right != null) deque.offer(cur.right);
-            }
-        }
-
-        return depth;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return (left == 0 || right == 0) ? left + right + 1 : Math.min(left, right) + 1;
     }
 }
