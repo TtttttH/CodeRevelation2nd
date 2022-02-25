82065bdbb590c84dd8e7aa5bd9fbadf8b4a87fba
TtttttH <574173437@qq.com>
2022-02-25 14:04:33 +0000

    add class Node into No_429

--- src/Tree/No_429.java
+++ src/Tree/No_429.java
@@ -35,3 +35,17 @@ public class No_429 {
 }
 
 
+class Node {
+    public int val;
+    public List<Tree.Node> children;
+
+    public Node(){}
+    public Node(int val) {
+        this.val = val;
+    }
+
+    public Node(int val, List<Tree.Node> children) {
+        this.val = val;
+        this.children = children;
+    }
+}
