4d18333d04f9889e44caf633bb3de6e9d58049fa
TtttttH <574173437@qq.com>
2022-02-25 17:50:19 +0000

    No.104 二叉树的深度 优化一下递归

--- src/Tree/No_104.java
+++ src/Tree/No_104.java
@@ -5,14 +5,18 @@ package Tree;
  */
 public class No_104 {
     public int maxDepth(TreeNode root) {
-        return maxDepth(root, 0);
-    }
-
-    private int maxDepth(TreeNode root, int depth) {
         if (root == null) {
-            return depth;
+            return 0;
         }
 
-        return Math.max(maxDepth(root.left, depth + 1), maxDepth(root.right, depth + 1));
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
+
+//    private int maxDepth(TreeNode root, int depth) {
+//        if (root == null) {
+//            return depth;
+//        }
+//
+//        return Math.max(maxDepth(root.left, depth + 1), maxDepth(root.right, depth + 1));
+//    }
 }
