8836a6817c1650b70996364404ee9612410e3345
TtttttH <574173437@qq.com>
2022-03-01 20:56:05 +0000

    No.108 有序数组重建二叉搜索树

---
+++ src/Tree/No_108.java
@@ -0,0 +1,19 @@
+package Tree;
+
+/**
+ * No.108 Convert Sorted Array to Binary Search Tree (easy) Microsoft question
+ */
+public class No_108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+}
