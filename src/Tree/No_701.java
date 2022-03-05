c924e11bb922effb9631a596827708cbcdf04b53
TtttttH <574173437@qq.com>
2022-03-05 10:54:26 +0000

    No.701 二叉搜索树中的插入操作，迭代版本，思路更清晰

--- src/Tree/No_701.java
+++ src/Tree/No_701.java
@@ -4,24 +4,26 @@ package Tree;
  * No.701 二叉搜索树中的插入操作 (middle) 微软题
  */
 public class No_701 {
+//    public TreeNode insertIntoBST(TreeNode root, int val) {
+//        if (root == null) return new TreeNode(val);
+//        if (root.val > val) root.left = insertIntoBST(root.left, val);
+//        if (root.val < val) root.right = insertIntoBST(root.right, val);
+//        return root;
+//    }
+    //迭代法
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        helper(root, val);
+        if (root == null) return new TreeNode(val);
+        TreeNode cur = root;
+        TreeNode parent = null;
+        while (cur != null) {
+            parent = cur;
+            if (cur.val < val) cur = cur.right;
+            else cur = cur.left;
+        }
+
+        TreeNode node = new TreeNode(val);
+        if (parent.val < val) parent.right = node;
+        else parent.left = node;
         return root;
-    }
-
-
-    private void helper(TreeNode root, int val) {
-        if (root.val < val && root.right == null) {
-            root.right = new TreeNode(val);
-            return;
-        }
-
-        if (root.val > val && root.left == null) {
-            root.left = new TreeNode(val);
-            return;
-        }
-
-        if (root.val < val) helper(root.right, val);
-        else helper(root.left, val);
     }
 }
