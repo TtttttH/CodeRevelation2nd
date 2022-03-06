95f801a570502b9fab8f6e02f3ca059b6c1055b7
TtttttH <574173437@qq.com>
2022-03-06 12:25:15 +0000

    No.538 二叉搜索树转换为累加树,反向中序遍历，递归写法还是太不熟悉了，要尝试理解递归的思想

--- src/Tree/No_538.java
+++ src/Tree/No_538.java
@@ -7,22 +7,28 @@ import java.util.LinkedList;
  * No.538(1038) 把二叉搜索树转换为累加树 (middle)
  */
 public class No_538 {
+    private int pre = 0;
     public TreeNode convertBST(TreeNode root) {
         if (root == null) return null;
-        TreeNode cur = root;
-        int cumulative = 0;
-        Deque<TreeNode> stack = new LinkedList<>();
-        while (cur != null || !stack.isEmpty()) {
-            while (cur != null) {
-                stack.push(cur);
-                cur = cur.right;
-            }
-            cur = stack.pop();
-            cur.val += cumulative;
-            cumulative = cur.val;
-            cur = cur.left;
-        }
-
+        convertBST(root.right);
+        root.val += pre;
+        pre = root.val;
+        convertBST(root.left);
         return root;
+//        TreeNode cur = root;
+//        int cumulative = 0;
+//        Deque<TreeNode> stack = new LinkedList<>();
+//        while (cur != null || !stack.isEmpty()) {
+//            while (cur != null) {
+//                stack.push(cur);
+//                cur = cur.right;
+//            }
+//            cur = stack.pop();
+//            cur.val += cumulative;
+//            cumulative = cur.val;
+//            cur = cur.left;
+//        }
+//
+//        return root;
     }
 }
