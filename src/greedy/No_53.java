3c79799e5868a6f0e667b3661135f9f58edd58b5
TtttttH <574173437@qq.com>
2022-03-13 13:22:21 +0000

    No.53 最大子数组 补充假如返回子数组元素方法，设置左边界右边界，以及tempLeft，贪心算法

--- src/greedy/No_53.java
+++ src/greedy/No_53.java
@@ -1,5 +1,8 @@
 package greedy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * No.53 最大子数组和 (easy) 微软
  */
@@ -13,5 +16,39 @@ public class No_53 {
         }
 
         return max;
+    }
+
+    //进阶，返回最大子数组
+    public List<Integer> maxSubArrayList(int[] nums) {
+        int left = 0 ,right = 0, tempLeft = 0, res = nums[0], cur = res;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i < nums.length; i ++) {
+            if (cur > 0) cur += nums[i];
+            else {
+                cur = nums[i];
+                tempLeft = i;
+            }
+
+            if (res <= cur) {
+                res = cur;
+                left = tempLeft;
+                right = i;
+            }
+        }
+
+        for (int i = left; i <= right; i ++) {
+            list.add(nums[i]);
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        No_53 test = new No_53();
+        List<Integer> list = test.maxSubArrayList(nums);
+        for (int i : list) {
+            System.out.printf(i + ".");
+        }
     }
 }
