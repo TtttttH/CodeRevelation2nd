b9dafa7109e23656740415514aa15b60a8f24039
TtttttH <574173437@qq.com>
2022-03-22 15:53:16 +0000

    No.198 打架劫舍，dp

---
+++ src/dp/No_198.java
@@ -0,0 +1,17 @@
+package dp;
+
+/**
+ * No.198 打家劫舍 (middle) 微软
+ */
+public class No_198 {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 1; i < nums.length; i ++) {
+            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+        }
+
+        return dp[nums.length + 1];
+    }
+}
